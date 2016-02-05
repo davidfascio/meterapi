e250306020c82fe5a17b920b9d4d30ca08260899
David Fascio <david.fascio@gmail.com>
2016-02-05 09:37:47 -0600

    Ktron Meter Interface Library

---
+++ KitronMeterInterface.c
@@ -0,0 +1,96 @@
+#include "KitronMeterInterface.h"
+
+COMMAND_ID_FUNCTION KitronMeterInterface_CommandIdFunctionList [] = 
+{
+    {   READ_MODE ,                                         /* Meter Common Command Id              */
+        KitronMeterInterface_ReadMeteringData } ,             /* Meter CommandIdFunction_Callback     */
+        
+    COMMAND_ID_FUNCTION_NULL
+};
+
+//******************************************************************************
+// G155 Meter Interface API
+//******************************************************************************
+BOOL KitronMeterInterface_BuildFrame(   BYTE modbusId,                                        
+                                        BYTE commandFunction,
+                                        WORD registerAddress, 
+                                        WORD registerAmount, 
+                                        BYTE * data, 
+                                        BYTE dataLen, 
+                                        BYTE * frame, 
+                                        BYTE * frameLen){
+    
+    BYTE * frame_ptr = frame;
+    WORD crcFrame;
+    
+    memcpy(frame_ptr, &modbusId, KITRON_METER_INTERFACE_MODBUS_ID_HEADER_SIZE);
+    frame_ptr += KITRON_METER_INTERFACE_MODBUS_ID_HEADER_SIZE;
+    
+    memcpy(frame_ptr, &commandFunction, KITRON_METER_INTERFACE_COMMAND_FUNCTION_HEADER_SIZE);
+    frame_ptr +=  KITRON_METER_INTERFACE_COMMAND_FUNCTION_HEADER_SIZE;
+    
+    inverted_memcpy(frame_ptr, (BYTE *) &registerAddress, KITRON_METER_INTERFACE_REGISTER_ADDRESS_HEADER_SIZE);
+    frame_ptr += KITRON_METER_INTERFACE_REGISTER_ADDRESS_HEADER_SIZE;
+    
+    inverted_memcpy(frame_ptr, (BYTE *) &registerAmount, KITRON_METER_INTERFACE_REGISTER_AMOUNT_HEADER_SIZE);
+    frame_ptr += KITRON_METER_INTERFACE_REGISTER_AMOUNT_HEADER_SIZE;
+    
+    switch(commandFunction){
+        
+        case KITRON_METER_INTERFACE_READ_COMMAND_FUNCTION:            
+            break;
+            
+        default:
+            return FALSE;
+    }
+    
+    // Calculing CRC Value
+    crcFrame = wfnCRC_CALC((BYTE *) frame, (frame_ptr - frame), KITRON_METER_INTERFACE_CRC_BASE);
+    
+    // Adding CRC into frame buffer
+    memcpy(frame_ptr, (BYTE *) &crcFrame, KITRON_METER_INTERFACE_CRC_HEADER_SIZE);
+    frame_ptr += KITRON_METER_INTERFACE_CRC_HEADER_SIZE;
+    
+    * frameLen = (BYTE) (frame_ptr - frame);
+    
+    return TRUE;    
+}
+
+void KitronMeterInterface_SendFrame( BYTE modbusId,                                     
+                                     BYTE commandFunction,
+                                     WORD registerAddress, 
+                                     WORD registerAmount, 
+                                     BYTE * data, 
+                                     BYTE dataLen ){
+    
+    BYTE frame[KITRON_METER_INTERFACE_MAX_FRAME_BUFFER_SIZE];
+    BYTE frameLen;
+    BOOL framebuilt;
+    
+    framebuilt = KitronMeterInterface_BuildFrame(   modbusId,           /* modbusId         */
+                                                    commandFunction,    /* commandFunction  */     
+                                                    registerAddress,    /* registerAddress, */
+                                                    registerAmount,     /* registerAmount   */
+                                                    data,               /* data             */
+                                                    dataLen,            /* dataLen          */
+                                                    frame,              /* frame            */
+                                                    &frameLen);         /* frameLen         */
+    
+    if(framebuilt == FALSE)
+        return;
+    //!print_info("Sent Data: ");   
+    //! ComSerialInterface_PrintData(frame, frameLen);        
+    ComSerialInterface_SendData(frame, frameLen);        
+}
+
+void KitronMeterInterface_ReadMeteringData(BYTE modbusId, BYTE * serialNumber, WORD serialNumberLen, BYTE * data, WORD dataLen){
+    
+    print_info("Requesting METERING Command");
+    
+    KitronMeterInterface_SendFrame( modbusId,                                     
+                                    KITRON_METER_INTERFACE_READ_COMMAND_FUNCTION,
+                                    KITRON_METER_INTERFACE_VOLTAGE_PHASE_A_REGISTER_ADDRESS,
+                                    KITRON_METER_INTERFACE_VOLTAGE_PHASE_A_REGISTER_ADDRESS_SIZE,
+                                    NULL,
+                                    0);      
+}
---
+++ KitronMeterInterface.h
@@ -0,0 +1,115 @@
+/* 
+ * File:   KitronMeterInterface.h
+ * Author: fascio
+ *
+ * Created on 4 de febrero de 2016, 13:51
+ */
+
+#ifndef __KITRON_METER_INTERFACE_H__
+#define	__KITRON_METER_INTERFACE_H__
+
+//******************************************************************************
+// Includes
+//******************************************************************************
+#include "Utility.h"
+#include "MeterCommons.h"
+#include "ComSerialInterface.h"
+
+//******************************************************************************
+// Defines
+//******************************************************************************
+#define KITRON_METER_INTERFACE_MODBUS_ID_HEADER_SIZE                        (1)
+#define KITRON_METER_INTERFACE_COMMAND_FUNCTION_HEADER_SIZE                 (1)
+#define KITRON_METER_INTERFACE_REGISTER_ADDRESS_HEADER_SIZE                 (2)
+#define KITRON_METER_INTERFACE_REGISTER_AMOUNT_HEADER_SIZE                  (2)
+#define KITRON_METER_INTERFACE_CRC_HEADER_SIZE                              (2)
+
+#define KITRON_METER_INTERFACE_READ_COMMAND_FUNCTION                      (0x03)
+#define KITRON_METER_INTERFACE_CRC_BASE                                   (0xFFFF)
+
+#define KITRON_METER_INTERFACE_MAX_FRAME_BUFFER_SIZE                        (50)
+
+//******************************************************************************
+// KITRON METER INTERFACE REGISTER ADDRESSES
+//******************************************************************************
+#define KITRON_METER_INTERFACE_VOLTAGE_PHASE_A_REGISTER_ADDRESS         (0x6611)
+#define KITRON_METER_INTERFACE_VOLTAGE_PHASE_A_REGISTER_ADDRESS_SIZE         (1)
+
+#define KITRON_METER_INTERFACE_VOLTAGE_PHASE_B_REGISTER_ADDRESS         (0x6612)
+#define KITRON_METER_INTERFACE_VOLTAGE_PHASE_B_REGISTER_ADDRESS_SIZE         (1)
+
+#define KITRON_METER_INTERFACE_VOLTAGE_PHASE_C_REGISTER_ADDRESS         (0x6613)
+#define KITRON_METER_INTERFACE_VOLTAGE_PHASE_C_REGISTER_ADDRESS_SIZE         (1)
+
+#define KITRON_METER_INTERFACE_CURRENT_PHASE_A_REGISTER_ADDRESS         (0x6621)
+#define KITRON_METER_INTERFACE_CURRENT_PHASE_A_REGISTER_ADDRESS_SIZE         (1)
+
+#define KITRON_METER_INTERFACE_CURRENT_PHASE_B_REGISTER_ADDRESS         (0x6622)
+#define KITRON_METER_INTERFACE_CURRENT_PHASE_B_REGISTER_ADDRESS_SIZE         (1)
+
+#define KITRON_METER_INTERFACE_CURRENT_PHASE_C_REGISTER_ADDRESS         (0x6623)
+#define KITRON_METER_INTERFACE_CURRENT_PHASE_C_REGISTER_ADDRESS_SIZE         (1)
+
+#define KITRON_METER_INTERFACE_TOTAL_INSTANTANEOUS_ACTIVE_POWER_REGISTER_ADDRESS        (0x6630)
+#define KITRON_METER_INTERFACE_TOTAL_INSTANTANEOUS_ACTIVE_POWER_REGISTER_ADDRESS_SIZE       (1)
+
+#define KITRON_METER_INTERFACE_ACTIVE_POWER_PHASE_A_REGISTER_ADDRESS    (0x6631)
+#define KITRON_METER_INTERFACE_ACTIVE_POWER_PHASE_A_REGISTER_ADDRESS_SIZE    (1)
+
+#define KITRON_METER_INTERFACE_ACTIVE_POWER_PHASE_B_REGISTER_ADDRESS    (0x6632)
+#define KITRON_METER_INTERFACE_ACTIVE_POWER_PHASE_B_REGISTER_ADDRESS_SIZE    (1)
+
+#define KITRON_METER_INTERFACE_ACTIVE_POWER_PHASE_C_REGISTER_ADDRESS    (0x6633)
+#define KITRON_METER_INTERFACE_ACTIVE_POWER_PHASE_C_REGISTER_ADDRESS_SIZE    (1)
+
+#define KITRON_METER_INTERFACE_TOTAL_POWER_FACTOR_REGISTER_ADDRESS      (0x6650)
+#define KITRON_METER_INTERFACE_TOTAL_POWER_FACTOR_REGISTER_ADDRESS_SIZE      (1)
+
+#define KITRON_METER_INTERFACE_POWER_FACTOR_PHASE_A_REGISTER_ADDRESS    (0x6651)
+#define KITRON_METER_INTERFACE_POWER_FACTOR_PHASE_A_REGISTER_ADDRESS_SIZE    (1)
+
+#define KITRON_METER_INTERFACE_POWER_FACTOR_PHASE_B_REGISTER_ADDRESS    (0x6652)
+#define KITRON_METER_INTERFACE_POWER_FACTOR_PHASE_B_REGISTER_ADDRESS_SIZE    (1)
+
+#define KITRON_METER_INTERFACE_POWER_FACTOR_PHASE_C_REGISTER_ADDRESS    (0x6653)
+#define KITRON_METER_INTERFACE_POWER_FACTOR_PHASE_C_REGISTER_ADDRESS_SIZE    (1)
+
+#define KITRON_METER_INTERFACE_TOTAL_INSTANTANEOUS_APPARENT_POWER_REGISTER_ADDRESS    (0x6660)
+#define KITRON_METER_INTERFACE_TOTAL_INSTANTANEOUS_APPARENT_POWER_REGISTER_ADDRESS_SIZE    (1)
+
+#define KITRON_METER_INTERFACE_APPARENT_POWER_PHASE_A_REGISTER_ADDRESS      (0x6661)
+#define KITRON_METER_INTERFACE_APPARENT_POWER_PHASE_A_REGISTER_ADDRESS_SIZE     (1)
+
+#define KITRON_METER_INTERFACE_APPARENT_POWER_PHASE_B_REGISTER_ADDRESS      (0x6662)
+#define KITRON_METER_INTERFACE_APPARENT_POWER_PHASE_B_REGISTER_ADDRESS_SIZE     (1)
+
+#define KITRON_METER_INTERFACE_APPARENT_POWER_PHASE_C_REGISTER_ADDRESS      (0x6663)
+#define KITRON_METER_INTERFACE_APPARENT_POWER_PHASE_C_REGISTER_ADDRESS_SIZE     (1)
+
+//******************************************************************************
+// G155 Meter Interface API
+//******************************************************************************
+BOOL KitronMeterInterface_BuildFrame(   BYTE modbusId,                                        
+                                        BYTE commandFunction,
+                                        WORD registerAddress, 
+                                        WORD registerAmount, 
+                                        BYTE * data, 
+                                        BYTE dataLen, 
+                                        BYTE * frame, 
+                                        BYTE * frameLen);
+
+void KitronMeterInterface_SendFrame( BYTE modbusId,                                     
+                                     BYTE commandFunction,
+                                     WORD registerAddress, 
+                                     WORD registerAmount, 
+                                     BYTE * data, 
+                                     BYTE dataLen );
+
+void KitronMeterInterface_ReadMeteringData(BYTE modbusId, BYTE * serialNumber, WORD serialNumberLen, BYTE * data, WORD dataLen);
+
+extern COMMAND_ID_FUNCTION KitronMeterInterface_CommandIdFunctionList [];
+
+
+
+#endif	/* __KITRON_METER_INTERFACE_H__ */
+
